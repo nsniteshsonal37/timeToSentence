354fe52057dbc1fc285769070bf5c43ecfaa0536
nsniteshsonal37 <nsniteshsonal37@gmail.com>
2022-07-26 05:48:20 -0500

    disable regex

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from dataclasses import InitVar
 from fastapi import FastAPI, Form
 import uvicorn
 from pydantic import BaseModel
 from runner import *
 from fastapi.middleware.cors import CORSMiddleware
 import re
-import gunicorn
 app=FastAPI() 
 regex= '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
 x="01:60"
@@ -29,20 +27,20 @@ def postF(time: str=Form()):
     global inputTime, var
     inputTime=time
     var=re.match(regex,inputTime)
-    if(var):
-        return{"Status":"Received"}
-    else:
-        return{"Status":"Regex Fail"}
+   # if(var):
+    return{"Status":"Received"}
+    #else:
+     #   return{"Status":"Regex Fail"}
 @app.get("/")
 def root():
     return{"Status": "API Launched"}
 @app.get("/get")
 def root():
 
-    if(var):
-        return{"time": out(inputTime)}
-    else:
-        return{"Status":"Regex Fail"}
+    #if(var):
+    return{"time": out(inputTime)}
+    #else:
+     #   return{"Status":"Regex Fail"}
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", port=5000, log_level="info", reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info", reload=True)
