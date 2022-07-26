bed79187fa469b1e78adb3eed60ea7ea841b0038
nsniteshsonal37 <nsniteshsonal37@gmail.com>
2022-07-25 23:21:16 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Form
+import uvicorn
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from runner import *
+import re
+
+app=FastAPI() 
+regex= '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
+x="01:60"
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Time(BaseModel):
+    time: str
+
+@app.post("/post")
+def postF(time: str=Form()):
+    global inputTime, var
+    inputTime=time
+    var=re.match(regex,inputTime)
+    if(var):
+        return{"Status":"Received"}
+    else:
+        return{"Status":"Regex Fail"}
+@app.get("/")
+def root():
+    if(var):
+        return{"time": out(inputTime)}
+    else:
+        return{"Status":"Regex Fail"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info", reload=True)
---
+++ runner.py
@@ -0,0 +1,45 @@
+from num2words import num2words
+def num2string(num):
+    return((num2words(num)).replace('-',' '))
+def out(x):
+    list1=x.split(':')
+    s1=int(list1[0])
+    s2=int(list1[1])
+    if(s1<0):
+        return("A day can't have negative hours!")
+    if(s1==0):
+        s1=12
+    if(s1>12 and s1<25):
+        s1=int(s1/2)
+    if(s1>24):
+        return ("A day only has 24 hours! Please provide valid input!")
+    if(s2>60):
+        return("An hour only has 60 minutes!")
+    if(s2<0):
+        return("An hour can't have negative minutes!")
+    elif(s2==0):
+        return (f"{num2string(s1)} o{{}}clock".format("'"))
+    elif(s2==1):
+        return (f"one minute past {num2string(s1)}")
+    elif(s2==15):
+        return (f"quarter past {num2string(s1)}")
+    elif(s2==30):
+        return (f"half past {num2string(s1)}")
+    elif(s2==45):
+        if(s1==12):
+            return (f"quarter to {num2string(1)}")
+        return (f"quarter to {num2string(s1+1)}")
+    elif(s2==59):
+        if(s1==12):
+            return (f"one minute to {num2string(1)}")
+        return (f"one minute to {num2string(s1+1)}")
+    elif(s2==60):
+        if(s1==12):
+            return (f"{num2string(1)} o{{}}clock".format("'"))
+        return (f"{num2string(s1+1)} o{{}}clock".format("'"))
+    elif(s2>30):
+        if(s1==12):
+            return (f"{num2string(60-s2)} minutes to {num2string(1)}")
+        return (f"{num2string(60-s2)} minutes to {num2string(s1+1)}")
+    elif(s2<30):
+        return (f"{num2string(s2)} minutes past {num2string(s1)}")
